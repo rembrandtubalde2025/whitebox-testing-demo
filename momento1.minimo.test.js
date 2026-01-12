b0187c5d918622efb3831eacedfed3a69a534308
rembrandtubalde2025 <rembrandtubalde@gmail.com>
2026-01-12 12:05:15 -0500

    Update momento1.minimo.test.js

--- momento1.minimo.test.js
+++ momento1.minimo.test.js
@@ -1,4 +1,4 @@
-const { calcularDescuento } = require('./calculator.js');
+const { calcularDescuento } = require('./calculator');
 
 test('VIP con monto alto', () => {
   expect(calcularDescuento(1500, 'VIP')).toBe(450);
