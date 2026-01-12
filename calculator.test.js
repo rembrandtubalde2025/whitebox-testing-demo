7bb039bab51d2700fdd5c734cf6225cab19a16ef
rembrandtubalde2025 <rembrandtubalde@gmail.com>
2026-01-12 12:15:34 -0500

    Delete calculator.test.js

--- calculator.test.js
+++
@@ -1,6 +0,0 @@
-const { calcularDescuento } = require('./calculator');
-
-test('VIP con monto alto', () => {
-  const resultado = calcularDescuento(1500, 'VIP');
-  expect(resultado).toBe(450);
-});
