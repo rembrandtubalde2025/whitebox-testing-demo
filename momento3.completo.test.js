e50ccd50ddbcf4368ae36abbdbc9856d5e01749b
rembrandtubalde2025 <rembrandtubalde@gmail.com>
2026-01-12 12:06:19 -0500

    Create momento3.completo.test.js

---
+++ momento3.completo.test.js
@@ -0,0 +1,13 @@
+const { calcularDescuento } = require('./calculator');
+
+test('Monto inválido', () => {
+  expect(calcularDescuento(0, 'VIP')).toBe(0);
+});
+
+test('Cliente NUEVO', () => {
+  expect(calcularDescuento(1000, 'NUEVO')).toBe(50);
+});
+
+test('Tipo desconocido', () => {
+  expect(calcularDescuento(1000, 'OTRO')).toBe(0);
+});
